79e20c28ea02b51d273b61d49f5222deaf57c4d4
An Hung <anbeel191@gmail.com>
2023-11-02 09:11:18 +0700

    Set up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  const a = 'Hello';
+
+  console.log(a);
+  return (
+    <>
+      <h1 className='font-bold text-gray-300'>Hello</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { PluginOption } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), visualizer() as PluginOption],
+  test: {
+    environment: 'jsdom',
+    setupFiles: path.resolve(__dirname, './vitest.setup.js')
+  },
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+  }
+});
